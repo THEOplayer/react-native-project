96a75e927c23da82a389b8c742e61d5b58515c0a
thijsl <thijs.lowette@theoplayer.com>
2021-05-20 13:07:49 -0400

    Make 02-rn-basic compatible with React Native 0.63.4 and use package managers and license string to load the THEOplayer SDKs

--- 02-rn-basic/ios/THEOplayerView.swift
+++ 02-rn-basic/ios/THEOplayerView.swift
@@ -19,15 +19,13 @@ class THEOplayerView: UIView {
     }
 
     /*
-      If you want to use Google Ima set googleIMA in theoplayer config(set true googleIMA in the line below) and add 'integration: "google-ima"' in js ads declaration.
-      You can declarate in THEOplayer configuration builder default js and css paths by using cssPaths() and jsPaths()
-     
     let scripthPaths = [Bundle.main.path(forResource: "theoplayer", ofType: "js")].compactMap { $0 }
     let stylePaths = [Bundle.main.path(forResource: "theoplayer", ofType: "css")].compactMap { $0 }
  */
     let playerConfig = THEOplayerConfiguration(
         chromeless: false,
-        googleIMA: false
+        pip: nil,
+        license: "your_license_string"
       /* To add the custom JS and CSS path
         cssPaths: stylePaths
         jsPaths: scripthPaths
